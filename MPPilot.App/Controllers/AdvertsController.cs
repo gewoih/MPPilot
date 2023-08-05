9fbeea39d9906e16d33d0265424d2d0ecb217c0b
Nikita Ranenko <nranenko@bk.ru>
2023-08-06 01:20:36 +0700

    MP-66 Implemented adverts table

--- MPPilot.App/Controllers/AdvertsController.cs
+++ MPPilot.App/Controllers/AdvertsController.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MPBoom.Domain.Models.Advert;
-using MPBoom.Domain.Models;
 using MPBoom.Domain.Services.API;
 
 namespace MPPilot.App.Controllers
@@ -8,35 +7,28 @@ namespace MPPilot.App.Controllers
     public class AdvertsController : Controller
     {
         private readonly WildberriesService _wildberriesService;
-        private readonly IConfiguration _configuration;
-        private UserSettings? _userSettings;
-        private IEnumerable<Advert> _adverts;
 
-        public AdvertsController(WildberriesService wildberriesService, 
-            IConfiguration configuration) 
+        public AdvertsController(WildberriesService wildberriesService) 
         {
             _wildberriesService = wildberriesService;
-            _configuration = configuration;
-
             _wildberriesService.SetApiKey("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2Nlc3NJRCI6IjExYTEzYjJhLTBjY2ItNDhhYS04NjE1LTYyNDg3NmY4MzdjZSJ9.0Lhiz7X_SjLE-kOqXEJ7BEIVdH673sbpVMfuV9VyX5M");
         }
 
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var isValidApiKey = false;
-
-            if (isValidApiKey)
+            try
             {
                 var searchAdverts = await _wildberriesService.GetAdvertsAsync(type: AdvertType.Search);
                 var productPageAdverts = await _wildberriesService.GetAdvertsAsync(type: AdvertType.ProductPage);
 
-                _adverts = searchAdverts.Concat(productPageAdverts);
-
+                var _adverts = searchAdverts.Concat(productPageAdverts);
                 return View(_adverts);
             }
-            else
-                return View("APIKeyError");
+            catch
+            {
+                return View(Enumerable.Empty<Advert>());
+            }
         }
     }
 }
