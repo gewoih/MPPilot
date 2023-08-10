d290fd3400662a7d2faeb1a6c971661d03cc76f1
Nikita Ranenko <nranenko@bk.ru>
2023-08-11 01:40:53 +0700

    MP-105 Minor changes

--- MPPilot.App/Controllers/AdvertsController.cs
+++ MPPilot.App/Controllers/AdvertsController.cs
@@ -5,75 +5,82 @@ using MPPilot.Domain.Services.Autobidders;
 
 namespace MPPilot.App.Controllers
 {
-    public class AdvertsController : Controller
-    {
-        private readonly WildberriesService _wildberriesService;
-        private readonly AccountsService _accountService;
-        private readonly AutobidderService _autobidderService;
+	public class AdvertsController : Controller
+	{
+		private readonly WildberriesService _wildberriesService;
+		private readonly AccountsService _accountService;
+		private readonly AutobidderService _autobidderService;
 
-        public AdvertsController(WildberriesService wildberriesService, AccountsService accountsService, AutobidderService autobidderService)
-        {
-            _wildberriesService = wildberriesService;
-            _accountService = accountsService;
-            _autobidderService = autobidderService;
-        }
+		public AdvertsController(WildberriesService wildberriesService, AccountsService accountsService, AutobidderService autobidderService)
+		{
+			_wildberriesService = wildberriesService;
+			_accountService = accountsService;
+			_autobidderService = autobidderService;
+		}
 
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            try
-            {
-                var accountSettings = await _accountService.GetCurrentAccountSettings();
+		[HttpGet]
+		public async Task<IActionResult> Index()
+		{
+			try
+			{
+				var adverts = await GetAdverts();
+				return View(adverts);
+			}
+			catch
+			{
+				return View(Enumerable.Empty<Advert>());
+			}
+		}
 
-                var adverts = new List<Advert>();
-                var searchAdvertsTask = _wildberriesService.GetAdvertsAsync(accountSettings.WildberriesApiKey, type: AdvertType.Search);
-                var productPageAdvertsTask = _wildberriesService.GetAdvertsAsync(accountSettings.WildberriesApiKey, type: AdvertType.ProductPage);
-                await Task.WhenAll(searchAdvertsTask, productPageAdvertsTask)
-                    .ContinueWith(task =>
-                    {
-                        foreach (var list in task.Result)
-                        {
-                            adverts.AddRange(list);
-                        }
-                    });
+		[HttpGet]
+		public async Task<List<Advert>> GetAdverts()
+		{
+			var accountSettings = await _accountService.GetCurrentAccountSettings();
 
-                //Заполнение автобиддеров, возможна оптимизация
-                adverts = adverts.OrderByDescending(advert => advert.LastUpdateDate).ToList();
-                foreach (var advert in adverts)
-                {
-                    advert.Autobidder = await _autobidderService.GetByAdvert(advert.AdvertId);
-                }
+			var adverts = new List<Advert>();
+			var searchAdvertsTask = _wildberriesService.GetAdvertsAsync(accountSettings.WildberriesApiKey, type: AdvertType.Search);
+			var productPageAdvertsTask = _wildberriesService.GetAdvertsAsync(accountSettings.WildberriesApiKey, type: AdvertType.ProductPage);
+			await Task.WhenAll(searchAdvertsTask, productPageAdvertsTask)
+				.ContinueWith(task =>
+				{
+					foreach (var list in task.Result)
+					{
+						adverts.AddRange(list);
+					}
+				});
 
-                return View(adverts);
-            }
-            catch
-            {
-                return View(Enumerable.Empty<Advert>());
-            }
-        }
+			//Заполнение автобиддеров, возможна оптимизация
+			adverts = adverts.OrderByDescending(advert => advert.LastUpdateDate).ToList();
+			foreach (var advert in adverts)
+			{
+				advert.Autobidder = await _autobidderService.GetByAdvert(advert.AdvertId);
+			}
 
-        [HttpPost]
-        public async Task<IActionResult> Edit(Advert oldAdvert, Advert newAdvert)
-        {
-            var accountSettings = await _accountService.GetCurrentAccountSettings();
-            var apiKey = accountSettings.WildberriesApiKey;
+			return adverts;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(Advert oldAdvert, Advert newAdvert)
+		{
+			var accountSettings = await _accountService.GetCurrentAccountSettings();
+			var apiKey = accountSettings.WildberriesApiKey;
 
 			if (oldAdvert.AdvertId != newAdvert.AdvertId)
-                throw new Exception($"Id старой РК ({oldAdvert.AdvertId}) и обновленной РК ({newAdvert.AdvertId}) не могут отличаться.");
+				throw new Exception($"Id старой РК ({oldAdvert.AdvertId}) и обновленной РК ({newAdvert.AdvertId}) не могут отличаться.");
 
-            var changeSettingsTasks = new List<Task>();
-            if (oldAdvert.Name != newAdvert.Name)
-                changeSettingsTasks.Add(_wildberriesService.RenameAdvert(apiKey, newAdvert.AdvertId, newAdvert.Name));
+			var changeSettingsTasks = new List<Task>();
+			if (oldAdvert.Name != newAdvert.Name)
+				changeSettingsTasks.Add(_wildberriesService.RenameAdvert(apiKey, newAdvert.AdvertId, newAdvert.Name));
 
-            if (oldAdvert.Keyword != newAdvert.Keyword)
-                changeSettingsTasks.Add(_wildberriesService.ChangeAdvertKeyword(apiKey, newAdvert.AdvertId, newAdvert.Keyword));
+			if (oldAdvert.Keyword != newAdvert.Keyword)
+				changeSettingsTasks.Add(_wildberriesService.ChangeAdvertKeyword(apiKey, newAdvert.AdvertId, newAdvert.Keyword));
 
-            if (oldAdvert.IsEnabled != newAdvert.IsEnabled)
-                changeSettingsTasks.Add(_wildberriesService.ChangeAdvertStatus(apiKey, newAdvert.AdvertId, newAdvert.Status));
+			if (oldAdvert.IsEnabled != newAdvert.IsEnabled)
+				changeSettingsTasks.Add(_wildberriesService.ChangeAdvertStatus(apiKey, newAdvert.AdvertId, newAdvert.Status));
 
-            await Task.WhenAll(changeSettingsTasks);
+			await Task.WhenAll(changeSettingsTasks);
 
-            return Json(new { success = true });
+			return Json(new { success = true });
 		}
-    }
+	}
 }
