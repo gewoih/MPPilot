5804bffd788a2061ddcd3a1052efd56c743c479f
Nikita Ranenko <nranenko@bk.ru>
2023-08-11 01:40:53 +0700

    MP-105 Minor changes

--- App/Controllers/AdvertsController.cs
+++ App/Controllers/AdvertsController.cs
@@ -5,18 +5,18 @@ using MPPilot.Domain.Services.Autobidders;
 
 namespace App.Controllers
 {
-    public class AdvertsController : Controller
-    {
-        private readonly WildberriesService _wildberriesService;
-        private readonly AccountsService _accountService;
-        private readonly AutobidderService _autobidderService;
+	public class AdvertsController : Controller
+	{
+		private readonly WildberriesService _wildberriesService;
+		private readonly AccountsService _accountService;
+		private readonly AutobidderService _autobidderService;
 
-        public AdvertsController(WildberriesService wildberriesService, AccountsService accountsService, AutobidderService autobidderService)
-        {
-            _wildberriesService = wildberriesService;
-            _accountService = accountsService;
-            _autobidderService = autobidderService;
-        }
+		public AdvertsController(WildberriesService wildberriesService, AccountsService accountsService, AutobidderService autobidderService)
+		{
+			_wildberriesService = wildberriesService;
+			_accountService = accountsService;
+			_autobidderService = autobidderService;
+		}
 
         [HttpGet]
         public async Task<IActionResult> Index()
@@ -63,26 +63,26 @@ namespace App.Controllers
             return adverts;
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Edit(Advert oldAdvert, Advert newAdvert)
-        {
-            var accountSettings = await _accountService.GetCurrentAccountSettings();
-            var apiKey = accountSettings.WildberriesApiKey;
+		[HttpPost]
+		public async Task<IActionResult> Edit(Advert oldAdvert, Advert newAdvert)
+		{
+			var accountSettings = await _accountService.GetCurrentAccountSettings();
+			var apiKey = accountSettings.WildberriesApiKey;
 
             if (oldAdvert.AdvertId != newAdvert.AdvertId)
                 throw new Exception($"Id старой РК ({oldAdvert.AdvertId}) и обновленной РК ({newAdvert.AdvertId}) не могут отличаться.");
 
-            var changeSettingsTasks = new List<Task>();
-            if (oldAdvert.Name != newAdvert.Name)
-                changeSettingsTasks.Add(_wildberriesService.RenameAdvert(apiKey, newAdvert.AdvertId, newAdvert.Name));
+			var changeSettingsTasks = new List<Task>();
+			if (oldAdvert.Name != newAdvert.Name)
+				changeSettingsTasks.Add(_wildberriesService.RenameAdvert(apiKey, newAdvert.AdvertId, newAdvert.Name));
 
-            if (oldAdvert.Keyword != newAdvert.Keyword)
-                changeSettingsTasks.Add(_wildberriesService.ChangeAdvertKeyword(apiKey, newAdvert.AdvertId, newAdvert.Keyword));
+			if (oldAdvert.Keyword != newAdvert.Keyword)
+				changeSettingsTasks.Add(_wildberriesService.ChangeAdvertKeyword(apiKey, newAdvert.AdvertId, newAdvert.Keyword));
 
-            if (oldAdvert.IsEnabled != newAdvert.IsEnabled)
-                changeSettingsTasks.Add(_wildberriesService.ChangeAdvertStatus(apiKey, newAdvert.AdvertId, newAdvert.Status));
+			if (oldAdvert.IsEnabled != newAdvert.IsEnabled)
+				changeSettingsTasks.Add(_wildberriesService.ChangeAdvertStatus(apiKey, newAdvert.AdvertId, newAdvert.Status));
 
-            await Task.WhenAll(changeSettingsTasks);
+			await Task.WhenAll(changeSettingsTasks);
 
             return Json(new { success = true });
         }
